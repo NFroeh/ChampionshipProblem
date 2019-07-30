ffeddb37adbee9527969db0d38995eb20a867236
Marth8 <nicolai.froehlig@gmail.com>
2019-07-30 17:29:12 +0200

    fix: made form scrollable.

--- ChampionshipProblem/ChampionshipProblemForm.Designer.cs
+++ ChampionshipProblem/ChampionshipProblemForm.Designer.cs
@@ -256,6 +256,7 @@ namespace ChampionshipProblem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(1548, 1022);
             this.Controls.Add(this.ResultGrid);
             this.Controls.Add(this.CountryComboBox);
