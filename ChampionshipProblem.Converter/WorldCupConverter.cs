60abbad5fc207e7d7693b07d6d35e20600de8b2f
Marth8 <nicolai.froehlig@gmail.com>
2019-08-02 11:26:19 +0200

    extensions: added excel files.

---
+++ ChampionshipProblem.Converter/WorldCupConverter.cs
@@ -0,0 +1,17 @@
+﻿namespace ChampionshipProblem.Converter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Klasse konvertiert die WorldCups in die vorgegebene Struktur.
+    /// </summary>
+
+    public class WorldCupConverter
+    {
+
+    }
+}
